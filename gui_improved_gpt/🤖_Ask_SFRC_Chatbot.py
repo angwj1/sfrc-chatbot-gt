51f292b20c2c4274bfc11b4165d5a6c2ff6a4949
angwj1 <86704698+angwj1@users.noreply.github.com>
2024-10-12 15:31:04 +0800

    update to gt api key

--- gui_improved_gpt/🤖_Ask_SFRC_Chatbot.py
+++ gui_improved_gpt/🤖_Ask_SFRC_Chatbot.py
@@ -48,11 +48,11 @@ if not check_password():
 
 ####### SET-UP #######
 # # set up openai api key and cohere api key
-os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY_GOVTECH']
+os.environ['OPENAI_API_KEY1'] = st.secrets['OPENAI_API_KEY_GOVTECH']
 os.environ['COHERE_API_KEY'] = st.secrets['COHERE_API_KEY']
 # embedding model that we will use for the session
 embeddings_model = OpenAIEmbeddings(
-    api_key=os.environ["OPENAI_API_KEY"],
+    api_key=os.environ["OPENAI_API_KEY1"],
     openai_api_base="https://litellm.govtext.gov.sg/",
     default_headers={"user-agent": "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0"},        
     model='text-embedding-3-large-prd-gcc2-lb', 
@@ -61,7 +61,7 @@ embeddings_model = OpenAIEmbeddings(
 
 # llm to be used in RAG pipeplines in this notebook
 llm = ChatOpenAI(
-    api_key=os.environ["OPENAI_API_KEY"],
+    api_key=os.environ["OPENAI_API_KEY1"],
     openai_api_base="https://litellm.govtext.gov.sg/",
     default_headers={"user-agent": "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0"},
     model = "gpt-4o-prd-gcc2-lb", 
